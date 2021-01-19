ea928ffbec535ee336a5f24423015b5628f83348
Jiawei Wu <abc1929@gmail.com>
2021-01-20 04:04:38 +0800

    mvp done?

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import Comments from '../Comments/Comments';
-import LikeSection from './LikeSection';
-import PostHeader from './PostHeader';
+import React from "react";
+import Comments from "../Comments/Comments";
+import LikeSection from "./LikeSection";
+import PostHeader from "./PostHeader";
 
-const Post = props => {
-  // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+const Post = (props) => {
+   // 🔥 Make sure the parent of Post is passing the right props!
+   const { post, likePost } = props;
 
-  return (
-    <div className='post-border'>
-      <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
-      />
-      <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
-        />
+   return (
+      <div className="post-border">
+         <PostHeader
+            username={post.username}
+            thumbnailUrl={post.thumbnailUrl}
+         />
+         <div className="post-image-wrapper">
+            <img
+               alt="post thumbnail"
+               className="post-image"
+               src={post.imageUrl}
+            />
+         </div>
+         {/* Is LikeSection getting all the props it needs to work correctly? */}
+         <LikeSection
+            likePost={() => likePost(post.id)}
+            numberOfLikes={post.likes}
+            postID={post.id}
+         />
+         {/* Comments also wants its props! */}
+         <Comments comments={post.comments} />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
-    </div>
-  );
+   );
 };
 
 export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+   // 🔥 Make sure the parent of Posts is passing the right props!
+   const { likePost, posts } = props;
 
-  return (
-    <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
-    </div>
-  );
+   return (
+      <div className="posts-container-wrapper">
+         {/* Map through the posts array returning a Post component at each iteration */}
+         {console.log(posts)}
+
+         {posts.map((post) => (
+            <Post key={post.id} post={post} likePost={likePost}></Post>
+         ))}
+
+         {/* Check the implementation of Post to see what props it requires! */}
+      </div>
+   );
 };
 
 export default Posts;
